0c6931a4b9fca101d937b4f61fe0a52d0aca6b85
MAB-100 <95566981+MAB-100@users.noreply.github.com>
2021-12-05 21:23:28 +0500

    Update templates.cpp

--- src/templates.cpp
+++ src/templates.cpp
@@ -1,10 +1,10 @@
 #include "BST.cpp"
 #include "BinaryTree.cpp"
-//
-//template class BST<int>;
-//template class BST<unsigned int>;
-//template class BST<float>;
-//template class BST<double>;
+
+template class BST<int>;
+template class BST<unsigned int>;
+template class BST<float>;
+template class BST<double>;
 
 template class BinaryTree<int>;
 template class BinaryTree<unsigned int>;
